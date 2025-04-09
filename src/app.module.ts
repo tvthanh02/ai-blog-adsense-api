fa70b279b733802ec73a52b6d7f7b094c8fcc25f
Trieu Thanh <tvthanh.cod@gmail.com>
2025-04-10 01:13:20 +0700

    add ssl config to connect to host db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthorModule } from './author/author.module';
 import { CategoryModule } from './category/category.module';
@@ -7,26 +7,39 @@ import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: +configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USER'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
-    AuthorModule,
-    CategoryModule,
-    PostModule,
-    CommentModule,
-  ],
+ imports: [
+  ConfigModule.forRoot({ isGlobal: true }),
+  TypeOrmModule.forRootAsync({
+   imports: [ConfigModule],
+   useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+    const baseConfig: TypeOrmModuleOptions = {
+     type: 'postgres',
+     host: configService.get('DATABASE_HOST'),
+     port: +configService.get('DATABASE_PORT'),
+     username: configService.get('DATABASE_USER'),
+     password: configService.get('DATABASE_PASSWORD'),
+     database: configService.get('DATABASE_NAME'),
+     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+     synchronize: true,
+    };
+
+    const sslSecure = configService.get('SSL_SECURE') === 'true';
+    return sslSecure
+     ? {
+      ...baseConfig,
+      ssl: {
+       require: true,
+       rejectUnauthorized: false,
+      },
+     }
+     : baseConfig;
+   },
+   inject: [ConfigService],
+  }),
+  AuthorModule,
+  CategoryModule,
+  PostModule,
+  CommentModule,
+ ],
 })
-export class AppModule {}
+export class AppModule { }
