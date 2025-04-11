90a0d0d0f5e3b3324bc92232be48b2d213697bbf
Trieu Thanh <tvthanh.cod@gmail.com>
2025-04-11 13:48:05 +0700

    fix file upload name

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { extname } from 'path';
 import * as fs from 'fs';
 import * as path from 'path';
 import { UploadController } from './upload.controller';
@@ -10,18 +9,21 @@ import { UploadService } from './upload.service';
 // Ensure uploads directory exists
 const uploadsDir = path.join(__dirname, '../../uploads');
 if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir, { recursive: true });
+ fs.mkdirSync(uploadsDir, { recursive: true });
 }
 
 @Module({
-  imports: [
-    MulterModule.register({
-      storage: diskStorage({
-        destination: uploadsDir,
-      }),
-    }),
-  ],
-  controllers: [UploadController],
-  providers: [UploadService],
+ imports: [
+  MulterModule.register({
+   storage: diskStorage({
+    destination: uploadsDir,
+    filename: (req, file, callback) => {
+     callback(null, `${file.originalname}`);
+    },
+   }),
+  }),
+ ],
+ controllers: [UploadController],
+ providers: [UploadService],
 })
-export class UploadModule {}
+export class UploadModule { }
